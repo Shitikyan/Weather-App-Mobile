4c3404723ff44adf9c87a9c7648c970f2798c47a
shitikyan <shitikyan@gmail.com>
2023-12-27 14:32:01 +0400

    implemented ui for search sectiona and added necessary images and icons

--- App.tsx
+++ App.tsx
@@ -1,11 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import AppNavigation from "./navigation/appNavigation";
 
 export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigation></AppNavigation>;
 }
